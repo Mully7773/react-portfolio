430208ae3100c8081a4edba267f026a3ceb51361
Nick M <mully7773@gmail.com>
2022-02-24 17:48:26 -0600

    Added links, home, and about page info

---
+++ src/components/About/index.js
@@ -0,0 +1,22 @@
+const About = () => {
+    return ( 
+<div className="container col-xxl-8 px-4 py-5" id="about-me">
+    <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div className="col-lg-6">
+        <h1 className="display-5 fw-bold lh-1 mb-3">About Me</h1>
+        <p className="lead">Hello, I'm Nick Mullenmeister. I am training to become a full-stack web developer. I am
+          enthusiastic about coding and I constantly strive to learn new technologies and implement them in my work.</p>
+        <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+        </div>
+      </div>
+      <div className="col-10 col-sm-8 col-lg-6">
+        <img src={`${process.env.PUBLIC_URL}/assets/joanna-kosinska-bF2vsubyHcQ-unsplash.jpg`} className="d-block mx-lg-auto img-fluid img-thumbnail rounded" alt="Picture of desk"
+          width="300" height="200" loading="lazy" />
+      </div>
+    </div>
+  </div>
+
+     );
+}
+ 
+export default About;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,14 +2,14 @@ const Home = () => {
     return ( <div className="container col-xxl-8 px-4 py-5" id="about-me">
     <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
       <div className="col-10 col-sm-8 col-lg-6">
-        <img src="" className="d-block mx-lg-auto img-fluid img-thumbnail rounded" alt="Picture of Nick"
+        <img src={`${process.env.PUBLIC_URL}/assets/nick.jpg`} className="d-block mx-lg-auto img-fluid img-thumbnail rounded" alt="Picture of Nick"
           width="300" height="200" loading="lazy" />
       </div>
       <div className="col-lg-6">
-        <h1 className="display-5 fw-bold lh-1 mb-3">About Me</h1>
-        <p className="lead">Hello, I'm Nick Mullenmeister. I am training to become a full-stack web developer. I am
-          enthusiastic about coding and I constantly strive to learn new technologies and implement them in my work.</p>
+        <h3 className="">Hi there! I'm</h3>
+        <h1 className="display-5 fw-bold lh-1 mb-3">Nick Mullenmeister</h1>
         <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+          <h3>Full Stack Web Developer</h3>
         </div>
       </div>
     </div>
